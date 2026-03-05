dafcbec5be3503535d64da3785d6378fcd19148d
Andrey Povstyanko <andrey.povstyanko@m-files.com>
2026-03-04 21:54:47 -0600

    feat: polish UI transitions

--- src/app/withdraw/ui/Button.tsx
+++ src/app/withdraw/ui/Button.tsx
@@ -12,7 +12,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         ref={ref}
         className={cn(
-          'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+          'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
           {
             'bg-primary text-primary-foreground hover:bg-primary/90': variant === 'default',
             'border border-input bg-background hover:bg-accent hover:text-accent-foreground':
